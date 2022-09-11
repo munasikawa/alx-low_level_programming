02fe04ed723b4ea120b2f03168554e02f0757b22
Munasikawa <munasikawa@gmail.com>
2022-09-11 09:52:15 -0700

    Commits Project 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 
-	char char;
+	char c;
 	int integer;
 	long int li;
 	long long int vli;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(integer));
 	printf("Size of a long int: %d bytes(s)\n", sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(vli));
