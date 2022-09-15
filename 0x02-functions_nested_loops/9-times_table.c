7a47772ea706a5df1006b453b3c5228ce2d9a616
Munasikawa <munasikawa@gmail.com>
2022-09-15 06:09:38 -0700

    fix code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,37 +11,26 @@ void times_table(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 10; b++)
+		for (b = 0; b < 10; b++)
 		{
 			c = a * b;
-			if ((c / 10) == 0)
+			if (b == 0)
+				_putchar(c + '0');
+			if (b != 0 && c < 10)
 			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
 			}
-			else
+			else if (c >= 10)
 			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }
