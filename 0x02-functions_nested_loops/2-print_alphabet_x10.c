e5b8c91997a79014762e258ff6e31236753d36cd
Munasikawa <munasikawa@gmail.com>
2022-09-15 03:42:44 -0700

    a function that prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 - prints the alphabet ten times 
+ *
+ *  Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int p;
+
+	while (p = 0; p < 10; p++)
+	{
+		while (i = 'a' ; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}
