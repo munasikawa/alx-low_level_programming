83545418c8447d9f791e1c6779898ffb44ff1892
Munasikawa <munasikawa@gmail.com>
2022-09-19 12:03:39 -0700

    fix task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,6 +4,7 @@
  * _puts - prints a string, followed by a new line
  *
  * @str: param
+ * Return: void
  */
 void _puts(char *str)
 {
@@ -11,5 +12,6 @@ void _puts(char *str)
 	{
 		_putchar(*str);
 	}
+
 	_putchar('\n');
 }
